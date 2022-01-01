828e0931d8f69975e429c9612c634a13bfe986d7
jcm2512 <joe.magalotti@gmail.com>
2022-01-01 19:17:20 +0900

    setDoc user

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,26 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import Login from "./login";
 import Loading from "./loading";
+import { useEffect } from "react";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore"; // V9
 
 function MyApp({ Component, pageProps }) {
   const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      setDoc(
+        doc(db, "users", user.uid),
+        {
+          email: user.email,
+          lastSeen: serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
+
   if (loading) return <Loading />;
   if (!user) return <Login />;
 
